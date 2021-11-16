81a9d45f05ab4658c6442d3f2da993d806da778d
Edgar <edgarlopez19082004@gmail.com>
2021-11-16 18:56:43 -0400

    urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views #we use views as auth_user because this is gonna separate our actual view file from this one
+
 from . import views
 
 urlpatterns = [
@@ -13,4 +16,19 @@ urlpatterns = [
     path('create_order/<str:pk>/', views.createOrder, name='create_order'),
     path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view()),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view()),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view()),   
+    
+    path('reset_password_complete//', auth_views.PasswordResetCompleteView.as_view()),
 ]
+
+'''
+1 - Submit email form                         //PasswordResetView.as_view()
+2 - Email sent success message                //PasswordResetDoneView.as_view()
+3 - Link to password Rest form in email       //PasswordResetConfirmView.as_view()
+4 - Password successfully changed message     //PasswordResetCompleteView.as_view()
+'''
